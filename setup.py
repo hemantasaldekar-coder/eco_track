19e288eb293afcef1e98613a945c96f060035b8d
hemantasaldekar-coder <hemantasaldekar@gmail.com>
2026-07-02 16:59:28 +0530

    Refactor setup.py to use setuptools
    Removed custom installation functions and integrated setup configuration.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eco_track",
+    version="0.0.1",
+    description="ESG and Carbon Footprint Tracking for ERPNext",
+    author="Developer",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+)
